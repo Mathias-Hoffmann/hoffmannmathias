70ca512c9b576579f3c1cfc69e877c56d12df30e
Mathias-Hoffmann <mathias.hoffmann@imt-atlantique.net>
2025-04-03 13:54:26 +0200

    🚀 Déploiement GitHub Pages

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Projets</h1>
+      <p>Voici quelques projets que j'ai réalisés.</p>
+    </main>
+  )
+}
